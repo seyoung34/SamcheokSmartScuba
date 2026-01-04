434775f84774fd70132ac03ff1578930baed1915
seyoung34 <jupi0304@naver.com>
2026-01-05 07:46:27 +0900

    overflow-hidden 삭제

--- app/components/DeepSea/DeepSeaScene.tsx
+++ app/components/DeepSea/DeepSeaScene.tsx
@@ -294,7 +294,7 @@ export default function DeepSeaScene() {
       <div style={{ height: "500vh" }} className="pointer-events-none" />
 
 
-      <div className="fixed inset-0 w-full h-full z-[-1] overflow-hidden bg-black">
+      <div className="fixed inset-0 w-full h-full z-[-1] bg-black">
         <Canvas
           camera={{ position: [0, 0, 10], fov: 75 }}
           gl={{ antialias: true, alpha: false }}
